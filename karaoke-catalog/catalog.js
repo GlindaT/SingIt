d0d42f264c5b7cb110ea96e2b4e1a195219621ef
GlindaT <caminante.via@gmail.com>
2026-05-12 00:19:11 -0500

    Add initial karaoke catalog with songs data

---
+++ karaoke-catalog/catalog.js
@@ -0,0 +1,17 @@
+{
+  "version": "1.0",
+  "songs": [
+    {
+      "id": "adele-when-we-were-young",
+      "title": "When We Were Young",
+      "artist": "Adele",
+      "folder": "adele-when-we-were-young"
+    },
+    {
+      "id": "kilombo-rozalen-sinmigo",
+      "title": "Invencibles",
+      "artist": "Mr Kilombo Y Rozalen",
+      "folder": "kilombo-rozalen-sinmigo"
+    }
+    ]
+}
