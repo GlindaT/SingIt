0fcc9cbf3d61512fbdd30f99e41181ba1e5c08e6
GlindaT <caminante.via@gmail.com>
2026-05-15 07:59:37 -0500

    Update Supabase configuration values

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://zpkearbtwumtbdgtezhq.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwa2VhcmJ0d3VtdGJkZ3RlemhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MTEzMTksImV4cCI6MjA5NDM4NzMxOX0.vtU6TrIctZPLDHMds1kB0yYHvYE1K9_NCFH-8HyuMkE"; 
+const SUPABASE_URL = "https://zpkearbtwumtbdgtezhq.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_F5IhJCYpGKthPomfbpMhrw_tjJNR0ed"; 
 
 // Esto es necesario para que el cliente se inicialice correctamente
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
