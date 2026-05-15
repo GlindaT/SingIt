8c30445eccb3db2a091a5875c7e31269f4e3ed9d
GlindaT <caminante.via@gmail.com>
2026-05-15 07:32:53 -0500

    Update SUPABASE_URL to include REST endpoint

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://zpkearbtwumtbdgtezhq.supabase.co";
+const SUPABASE_URL = "https://zpkearbtwumtbdgtezhq.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwa2VhcmJ0d3VtdGJkZ3RlemhxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODgxMTMxOSwiZXhwIjoyMDk0Mzg3MzE5fQ.6VqFxfn-6-ktLRxrbkcUntIYgGAVdevgQmm3D-c7zC8"; 
 
 // Esto es necesario para que el cliente se inicialice correctamente
