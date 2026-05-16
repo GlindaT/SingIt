a3a7cc343c46ac48363067efa6d02bcb4424b55e
GlindaT <caminante.via@gmail.com>
2026-05-15 19:41:49 -0500

    Change Supabase URL and anon key
    Updated Supabase URL and anonymous key for configuration.

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://zpkearbtwumtbdgtezhq.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_F5IhJCYpGKthPomfbpMhrw_tjJNR0ed"; 
+const SUPABASE_URL = "https://djwzneohwexymalnefhi.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_5CeiEjbsPTOFOkGbayXgvw_f-m8s2L9"; 
 
 // Esto es necesario para que el cliente se inicialice correctamente
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
