d284d0ca0b7331163fd0faf77c4309c9c89d3943
GlindaT <caminante.via@gmail.com>
2026-05-17 09:54:42 -0500

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://djwzneohwexymalnefhi.supabase.co";
+const SUPABASE_URL = "https://xcsjmehnnwimbyhalleb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhjc2ptZWhubndpbWJ5aGFsbGViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0OTAzMDgsImV4cCI6MjA5MjA2NjMwOH0.S23EhnHsmOkP1OfFLuoXTk1ITzqjYZ6BC3jWmpYaeps"; 
 
 // Esto es necesario para que el cliente se inicialice correctamente
