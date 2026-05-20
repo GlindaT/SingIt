8165a4a77232b7e3a3420be2ba935da8eba2ee75
GlindaT <caminante.via@gmail.com>
2026-05-20 11:08:37 -0500

    Rename Iniciar to startRecording for clarity

--- script.js
+++ script.js
@@ -2669,7 +2669,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     safeAdd("btnConfig", "click", () => showTab("config"));
 
     // afinador
-    safeAdd("recordBtn", "click", Iniciar);
+    safeAdd("recordBtn", "click", startRecording);
 
     // estudio
     safeAdd("audioFile", "change", cargarAudioEstudio);
