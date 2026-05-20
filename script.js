eaa956b2d0da5d69d5c09050247a1abbf1adcfd5
GlindaT <caminante.via@gmail.com>
2026-05-20 11:01:33 -0500

    Change 'iniciar' to 'Iniciar' for record button

--- script.js
+++ script.js
@@ -2669,7 +2669,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     safeAdd("btnConfig", "click", () => showTab("config"));
 
     // afinador
-    safeAdd("recordBtn", "click", iniciar);
+    safeAdd("recordBtn", "click", Iniciar);
 
     // estudio
     safeAdd("audioFile", "change", cargarAudioEstudio);
